4c2b29f55018f69f9628df2ecadd930091a4960d
Oleksandr <Chalyi>
2012-07-25 13:04:46 +0300

    Fix little problem with servlet

--- web/src/main/java/ua/edu/ChaliyLukyanov/laba3/controller/servlets/RemoveSingleComponentServlet.java
+++ web/src/main/java/ua/edu/ChaliyLukyanov/laba3/controller/servlets/RemoveSingleComponentServlet.java
@@ -8,7 +8,6 @@ import javax.ejb.FinderException;
 import javax.ejb.RemoveException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
